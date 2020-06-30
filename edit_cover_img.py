584430d4d4e9a85a13752047d3b2b575b284a80b
ParanoidAndroid19 <simranbhake.sb@gmail.com>
2020-07-01 02:22:46 +0530

    img error solved

--- edit_cover_img.py
+++ edit_cover_img.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 if __name__ == '__main__':
 
-    ori = epub.read_epub('test.epub')
+    ori = epub.read_epub('original.epub')
     ch_list = []
 
     # get metadata of original book
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # creating new book
     book = epub.EpubBook()
 
-    # calculating identifier
+    # calculating identifier for new book
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y%H%M%S")
 
@@ -40,11 +40,11 @@ if __name__ == '__main__':
     c2 = epub.EpubHtml(title='Cover Page', file_name='about.xhtml')
     c2.content='<p>Cover Image</p><p><img src="image.jpg" alt="Cover Image"/></p>'
 
-    # add chapters to the book
+    # add starting two chapters to the book
     book.add_item(c1)
     book.add_item(c2)
 
-    # adding my own content extracted from another book
+    # adding content (both text and imgs) extracted from original book
     i = 1
     for item in ori.get_items():
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
@@ -55,12 +55,19 @@ if __name__ == '__main__':
             book.add_item(ch)
             i = i + 1
 
+        if item.get_type() == ebooklib.ITEM_IMAGE:
+            b_img = item.get_content()
+            img_name = item.get_name()
+            img_id = item.get_id()
+            img_item = epub.EpubItem(uid=img_id, file_name=img_name, media_type='image/jpeg', content=b_img)
+            book.add_item(img_item)
+
+
     # create table of contents
     # - add manual link
     # - add section
     # - add auto created links to chapters
 
-# (epub.Link('intro.xhtml', 'Introduction', 'intro')
 
     book.toc = (epub.Link('intro.xhtml', 'Introduction', 'intro'), (epub.Section('Contents'), ch_list))
 
@@ -68,6 +75,7 @@ if __name__ == '__main__':
     # NOTE: Do not remove navigation, if you do then iBooks shows the epub as corrupt
     book.add_item(epub.EpubNcx())
     book.add_item(epub.EpubNav())
+
 
     # define css style
     style = '''
